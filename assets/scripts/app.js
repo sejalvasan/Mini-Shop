fc2621bd783a913d1fd07a747c90d39f7031329b
sejalvasan <sejalvasan@gmail.com>
2021-06-02 20:17:13 +0530

    constructor added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,17 +1,30 @@
+class Product{
+    title;
+    imageUrl;
+    description;
+    price;
+
+    constructor(title,image,desc,price){
+        this.title=title;
+        this.imageUrl=image;
+        this.description=desc;
+        this.price=price;
+    }
+}
 const productList ={
     products: [
-        {
-    title:'A Pillow',
-    imageUrl :'https://user-images.githubusercontent.com/69964629/120317238-0072c200-c2fc-11eb-9f27-477762280b85.jpg',
-    price: 19.99,
-    description:'A Soft Pillow!'
- },
- {
-    title:'A Carpet',
-    imageUrl :'https://user-images.githubusercontent.com/69964629/120320253-99570c80-c2ff-11eb-8f5e-39245b474d06.jpg',
-    price: 65.99,
-    description:'A Pebble Print Carpet!'
- }
+        new Product( 
+            'A Pillow',
+            'https://user-images.githubusercontent.com/69964629/120317238-0072c200-c2fc-11eb-9f27-477762280b85.jpg',
+            19.99,
+            'A Soft Pillow!'
+             ),
+    new Product(
+    'A Carpet',
+    'https://user-images.githubusercontent.com/69964629/120320253-99570c80-c2ff-11eb-8f5e-39245b474d06.jpg',
+     65.99,
+    'A Pebble Print Carpet!'
+    )
 ],
 render(){
     const renderHook = document.getElementById('app');
